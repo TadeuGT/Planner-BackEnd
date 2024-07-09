d54712c49a82933d3e0b39f8079a4c432ae8aaa9
TadeuGT <gabrieltadeugeromel@gmail.com>
2024-07-09 16:53:47 -0300

    Adicionadas rotas para atividades e links na API
    Aula 2 da NLW

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -1,16 +1,85 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import { dayjs } from "../lib/dayjs";
+import nodemailer from 'nodemailer';
 
 export async function confirmTrip(app: FastifyInstance) {
-    app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripID/confirm', {
+    app.withTypeProvider<ZodTypeProvider>().get(
+        '/trips/:tripId/confirm',
+        {
         schema: {
             params: z.object({
-                tripID: z.string().uuid(),
+                tripId: z.string().uuid(),
             })
         },
-    }, async (request) => {
+    },
+    async (request, reply) => {
+        const { tripId } = request.params
 
-        return { tripId: request.params.tripID }
+        const trip = await prisma.trip.findUnique({
+            where: {
+                id: tripId,
+            },
+            include: {
+                participants: {
+                    where: {
+                        is_owner: false,
+                    }
+                }
+            }
+        })
+
+        if (!trip) {
+            throw new Error('Viagem não encontrada!')
+        }
+
+        if (trip.is_confirmed) {
+            return reply.redirect(`http://localhost:3000/trips/${tripId}`)
+        }
+
+        await prisma.trip.update({
+            where: { id: tripId },
+            data: { is_confirmed: true},
+        })
+
+        const formattedStartDate = dayjs(trip.starts_at).format('LL')
+        const formattedEndDate = dayjs(trip.ends_at).format('LL')
+
+        const mail = await getMailClient()
+
+        //trip.participants
+        await Promise.all(
+            trip.participants.map(async (participant) => {
+                const confirmationLink = `http://localhost:3333/participants/${participant.id}/confirm`
+
+
+                const message = await mail.sendMail({
+                    from: {
+                        name: 'Equipe Plann.er',
+                        address: 'oi@plann.er',
+                    },
+                    to: participant.email,
+                    subject: `Confirme sua presença na viagem para ${trip.destination}`,
+                    html: `
+                    <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+                    <p>Você foi convidado para participar de uma viagem para <strong>${trip.destination}</strong>, nas datas de <strong>${formattedStartDate}</strong> até <strong>${formattedEndDate}</strong></p>
+                    <p></p>
+                    <p>Para confirmar sua presença na viagem, clique no link abaixo:</p>
+                    <p></p>
+                    <p><a href="${confirmationLink}">Confirmar viagem</a></p>
+                    <p></p>
+                    <p>Caso não saiba do que se trata esse e-mail, apenas ignore.</p>
+                    </div>
+                    `.trim()
+                })
+        
+                console.log(nodemailer.getTestMessageUrl(message))
+            })
+        )
+
+        return reply.redirect(`http://localhost:3000/trips/${tripId}`)
     })
 }
---
+++ src/routes/create-activity.ts
@@ -0,0 +1,49 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from 'zod'
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
+
+
+export async function createActivity(app: FastifyInstance) {
+    app.withTypeProvider<ZodTypeProvider>().post('/trips/:tripId/activities', {
+        schema: {
+            params: z.object({
+                tripId: z.string().uuid(),
+            }),
+            body: z.object({
+                title: z.string().min(4),
+                occurs_at: z.coerce.date(),
+            })
+        }
+    }, async (request) => {
+        const { tripId } = request.params
+        const { title, occurs_at } = request.body
+
+        const trip = await prisma.trip.findUnique({
+            where: { id: tripId }
+        })
+        
+        if (!trip) {
+            throw new Error("Viagem não encontrada.")
+        }
+
+        if (dayjs(occurs_at).isBefore(trip.starts_at)) {
+            throw new Error("Data de atividade anterior à data de viagem!")
+        }
+
+        if (dayjs(occurs_at).isAfter(trip.ends_at)) {
+            throw new Error("Data de atividade posterior à data de viagem!")
+        }
+
+        const activity = await prisma.activity.create({
+            data: {
+                title,
+                occurs_at,
+                trip_id: tripId,
+            }
+        })
+
+        return { activityId: activity.id }
+    })
+}
